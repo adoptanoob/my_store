1dcd84d9cc2f6aeccb4e2742cfbee16a62029bd4
adoptanoob <adoptanoobinrails@gmail.com>
2014-09-25 15:37:47 +0200

    changed initializer/spree.rb

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,3 +1,4 @@
+
 # Configure Spree Preferences
 #
 # Note: Initializing preferences available within the Admin will overwrite any changes that were made through the user interface when you restart.
@@ -9,6 +10,11 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+config.assets.precompile += %w( bx_loader.gif )
+config.assets.precompile << %w( store/print.css )
+config.assets.precompile << %w( icons.svg icons.ttf icons.eot icons.woff )
+config.assets.precompile << %w( images/bx_loader.gif images/controls.png )
+
 end
 Spree::AppConfiguration.preference :theme_background_color, :string, default: "#FFFFFF"
 
